152f23d773ccc92015b8bc523b727b7193cf38e3
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-06-10 14:46:50 +0700

    Initial Commit

---
+++ chordfit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chordfit
+//
+//  Created by Daniel Santoso on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
