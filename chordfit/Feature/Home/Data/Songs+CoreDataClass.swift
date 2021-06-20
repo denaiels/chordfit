0edd728faf480479258decefbb07d6f406ec2f65
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-06-21 01:12:28 +0700

    Add Home punya Grace

---
+++ chordfit/Feature/Home/Data/Songs+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Songs+CoreDataClass.swift
+//  chordfit
+//
+//  Created by Graciela gabrielle Angeline on 17/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Songs)
+public class Songs: NSManagedObject {
+
+}
---
+++ chordfit/Feature/Home/Model/ChordProgressionRepository.swift
@@ -0,0 +1,12 @@
+//
+//  ChordProgressionRepository.swift
+//  chordfit
+//
+//  Created by Graciela gabrielle Angeline on 16/06/21.
+//
+
+import UIKit
+
+class ChordProgressionRepository{
+    var progressionCategory: [SongProgression]?
+}
