5e49702e7050146dc16c8221115083a8891776fa
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-06-20 21:04:12 +0700

    Lanjut sambung songselection ke gameplay

--- chordfit/Feature/GameScene/Controller/GameSceneViewController.swift
+++ chordfit/Feature/GameScene/Controller/GameSceneViewController.swift
@@ -11,42 +11,45 @@ import GameplayKit
 
 class GameSceneViewController: UIViewController {
 
+    var songToPlay: Song?
+    var baseKey: String? = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene"){
-                // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
-           
-                // Present the scene
-            view.presentScene(scene)
-            
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-            
-        }
-        }
+        
+        let view = self.view as! SKView?
+
+        let scene = GameScene()
+//        let scene = SKScene(fileNamed: "GameScene")
+        
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene
+        view?.presentScene(scene)
+
+        view?.ignoresSiblingOrder = true
+        view?.showsFPS = true
+        view?.showsNodeCount = true
+        
 //        if let view = self.view as! SKView? {
+//
 //            // Load the SKScene from 'GameScene.sks'
-//            let scene = GameScene(size: view.bounds.size)
+//            if let scene = SKScene(fileNamed: "GameScene"){
+//
 //                // Set the scale mode to scale to fit the window
 //                scene.scaleMode = .aspectFill
+//
+//
 //
 //                // Present the scene
 //                view.presentScene(scene)
 //
-//
-//            view.ignoresSiblingOrder = true
-//
-//            view.showsFPS = true
-//            view.showsNodeCount = true
+//                view.ignoresSiblingOrder = true
+//                view.showsFPS = true
+//                view.showsNodeCount = true
+//            }
 //        }
-//        let scene : GameScene = GameScene(size: skview.frame.size)
-//        skview.presentScene(scene)
+        
     }
 }
